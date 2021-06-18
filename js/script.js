c821f1897653e21bc19a0969c6ee2b9d007d17cd
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-06-19 00:46:03 +0300

    add tooglr btween the pictures and the descriptions in #skills

--- js/script.js
+++ js/script.js
@@ -1,6 +1,41 @@
 $(document).ready(function(){
     $('#pic').click(function(){
-        $('#pop').show();
-        $('#pop').toggle();
+       $('#pup').toggle();
+       $('#pic').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#pup').click(function(){
+        $('#pic').toggle();
+        $('#pup').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#poc').click(function(){
+        $('#pob').toggle();
+        $('#poc').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#pob').click(function(){
+        $('#poc').toggle();
+        $('#pob').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#puc').click(function(){
+       $('#pog').toggle();
+       $('#puc').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#pog').click(function(){
+       $('#puc').toggle();
+       $('#pog').hide();
     });
 });
